8771acc2c5c51ce2bd0dea132ece75df54418b6c
Hubert Mądrzejewski <hubert.madrzejewski@gmail.com>
2024-07-05 20:44:23 +0200

    Pacman movement (directions and rotation)

--- Assets/Scripts/Pacman.cs
+++ Assets/Scripts/Pacman.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+
+[RequireComponent(typeof(Movement))]
 
 public class Pacman : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public Movement movement { get; private set; }
+
+    private void Awake()
     {
-        
+        movement = GetComponent<Movement>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            movement.SetDirection(Vector2.up);
+        }
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            movement.SetDirection(Vector2.down);
+        }
+        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            movement.SetDirection(Vector2.left);
+        }
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            movement.SetDirection(Vector2.right);
+        }
+
+        float angle = (float)Math.Atan2(movement.direction.y, movement.direction.x);
+        transform.rotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.forward);
     }
 }
