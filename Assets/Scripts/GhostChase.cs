222229a3bbf17093b29425ca3b930a51ea352fce
Hubert Mądrzejewski <hubert.madrzejewski@gmail.com>
2024-07-06 13:52:25 +0200

    Ghost chase:

--- Assets/Scripts/GhostChase.cs
+++ Assets/Scripts/GhostChase.cs
@@ -1,6 +1,34 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GhostChase : GhostBehaviour
 {
+  private void OnDisable()
+  {
+    ghost.scatter.Enable();
+  }
+  private void OnTriggerEnter2D(Collider2D other)
+  {
+    Node node = other.GetComponent<Node>();
 
+    if (node != null && enabled && !ghost.frightened.enabled)
+    {
+      Vector2 direction = Vector2.zero;
+      float minDistance = float.MaxValue;
+
+      foreach (Vector2 availableDirection in node.availableDirections)
+      {
+        Vector3 newPosition = transform.position + new Vector3(availableDirection.x, availableDirection.y, 0f);
+        float distance = (ghost.target.position - newPosition).sqrMagnitude;
+
+        if (distance < minDistance)
+        {
+          direction = availableDirection;
+          minDistance = distance;
+        }
+      }
+
+      ghost.movement.SetDirection(direction);
+    }
+  }
 }
