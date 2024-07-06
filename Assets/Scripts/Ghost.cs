9f0d045e34549193d0aaee0678afaa4952b5a0d7
Hubert Mądrzejewski <hubert.madrzejewski@gmail.com>
2024-07-06 02:24:35 +0200

    Init of ghost states

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -2,5 +2,24 @@ using UnityEngine;
 
 public class Ghost : MonoBehaviour
 {
+    public Movement movement { get; private set; }
+    public GhostHome home { get; private set; }
+    public GhostScatter scatter { get; private set; }
+    public GhostChase chase { get; private set; }
+    public GhostFrightened frightened { get; private set; }
+    public GhostBehaviour initialBehavior;
+    public Transform target;
     public int points = 200;
+
+    private void Awake()
+    {
+        movement = GetComponent<Movement>();
+        home = GetComponent<GhostHome>();
+        scatter = GetComponent<GhostScatter>();
+        chase = GetComponent<GhostChase>();
+        frightened = GetComponent<GhostFrightened>();
+    }
+
+
+
 }
